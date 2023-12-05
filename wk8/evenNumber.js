0e5632d4c58891825499d1d6f3bb02d3ae36a3b9
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-12-05 08:52:17 +0100

    function that checks if a parameter is even

---
+++ wk8/evenNumber.js
@@ -0,0 +1,13 @@
+//A function that checks if the the input/parameter is an even number.
+function evenNumber(even) {
+    if (even % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(evenNumber(4));
+console.log(evenNumber(5));
+console.log(evenNumber(6));
+console.log(evenNumber(7));
