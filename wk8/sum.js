5911225d4bb20b7848f96b55c247af1d999465e9
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-12-05 09:35:37 +0100

    function that sum two numbers

--- wk8/sum.js
+++ wk8/sum.js
@@ -1,10 +1,14 @@
 //function that sum the value between two numbers
 function addNum(n1, n2) {
-    let add = 20;
+    let add = 20;  //initial value
     for (let i = n1; i <= n2; i++) {    //iterating from n1 to n2
-        add = add + i;
+        add = add + i;              /*In the loop:
+                                add = add + 2 = 22 (when i = 2)
+                                add = add + 3 = 25 (when i = 3)
+                                add = add + 4 = 29 (when i = 4)*/
     }
     return add;
 }
 
 console.log(addNum(2, 4));
+
