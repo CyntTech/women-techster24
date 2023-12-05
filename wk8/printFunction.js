5c88334834747ce6ea2626c0e6fe2c29e6361765
Cynthia Amaji <cynthyok4luv@gmail.com>
2023-12-05 09:05:37 +0100

    function that prints to the console n1 to n2

---
+++ wk8/printFunction.js
@@ -0,0 +1,8 @@
+//function that prints to the console n1 to n2
+function printNum(n1, n2) {
+    for (let i = n1; i <= n2; i++) {    //iterating from n1 to n2
+        console.log(i);
+    }
+}
+
+printNum(6, 9);
